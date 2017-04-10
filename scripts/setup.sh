4caf08f602fc54c67d3f4430f3803b116f008b04
Vikash Kothary <kothary.vikash@gmail.com>
2017-04-10 14:23:02 +0100

    Move files into folders

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,23 +14,27 @@ fi
 
 # Setup virtual environment
 virtualenv venv
-source venv/bin/activate
+. venv/bin/activate
 
 # Install MyRobotLab
-FILENAME='myrobotlab.jar'
 LATEST_VERSION="1.0.1758"
+DIRECTORY="MyRobotLab"
+FILENAME="myrobotlab.jar"
+mkdir $DIRECTORY
+cd $DIRECTORY
 if [ ! -f $FILENAME ]; then
-
 	# Download latest MyRobotLab file
-    wget "https://github.com/MyRobotLab/myrobotlab/releases/download/${LATEST_VERSION}/${FILENAME}"
+    wget "https://github.com/MyRobotLab/myrobotlab/releases/download/$LATEST_VERSION/$FILENAME"
 
     # Install all required MyRobotLab services
-    java -jar $FILENAME -install OpenCV
+    java -jar $FILENAME -install ProgramAB MarySpeech WebkitSpeechRecognition OpenCV
 
     # Set project to run on boot
     # boot.sh
-
 fi
+cd ..
+
+# Move python scripts into folder
 
 # Run MyRobotLab
-java -jar $FILENAME
+#java -jar "$DIRECTORY/$FILENAME"
